37007d410fcc481f4a6ec61052b516bc26cc84e0
Brett <154654520+silencer7777@users.noreply.github.com>
2025-02-11 20:39:10 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementsByTagName("h1")[0].style.fontSize = "4vw";
 document.getElementsByTagName("p")[0].style.fontSize = "2vw";
 document.getElementsByTagName("p")[1].style.fontSize = "2vw";
 document.getElementsByTagName("p")[2].style.fontSize = "2vw";
+const boldText = document.querySelector('b');
+
+// Increase the font size
+boldText.style.fontSize = "2rem"; // You can adjust the size (e.g., "20px", "3vw", etc.)
