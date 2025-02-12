f12a0e9193b143c70fedeaf3cd80a4b65aff4c06
Brett <154654520+silencer7777@users.noreply.github.com>
2025-02-11 20:29:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "4vw";
 document.getElementsByTagName("p")[0].style.fontSize = "2vw";
+document.getElementsByTagName("p")[1].style.fontSize = "2vw";
+document.getElementsByTagName("p")[2].style.fontSize = "2vw";
