8b5a8fdec8188fe7fad266e9a82481208af14597
Brett <154654520+silencer7777@users.noreply.github.com>
2025-02-12 07:29:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ document.getElementsByTagName("p")[2].style.fontSize = "1vw";
 document.getElementsByTagName("p")[3].style.fontSize = "1vw";
 document.getElementsByTagName("p")[4].style.fontSize = "1vw";
 document.getElementsByTagName("p")[5].style.fontSize = "1vw";
+document.getElementsByTagName("p")[6].style.fontSize = "1vw";
+document.getElementsByTagName("p")[7].style.fontSize = "1vw";
+document.getElementsByTagName("p")[8].style.fontSize = "1vw";
+document.getElementsByTagName("p")[9].style.fontSize = "1vw";
+document.getElementsByTagName("p")[10].style.fontSize = "1vw";
 // Select the <b> element
 const boldText = document.querySelector('b');
 
