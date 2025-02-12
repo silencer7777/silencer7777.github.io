297e8c0b29722e493c85dea67a9b222f46c72a4a
Brett <154654520+silencer7777@users.noreply.github.com>
2025-02-12 07:04:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "4vw";
-document.getElementsByTagName("p")[0].style.fontSize = "2vw";
-document.getElementsByTagName("p")[1].style.fontSize = "2vw";
-document.getElementsByTagName("p")[2].style.fontSize = "2vw";
+document.getElementsByTagName("p")[0].style.fontSize = "1vw";
+document.getElementsByTagName("p")[1].style.fontSize = "1vw";
+document.getElementsByTagName("p")[2].style.fontSize = "1vw";
 const boldText = document.querySelector('b');
 
 // Increase the font size
